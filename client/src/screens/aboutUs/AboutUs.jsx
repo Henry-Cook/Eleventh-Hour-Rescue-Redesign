88f55e53e9454a5710eee6645aa1b00eb2fa0f28
ajmonda <ajmonda@gmail.com>
2020-08-21 11:06:56 -0400

    Return call

--- client/src/screens/aboutUs/AboutUs.jsx
+++ client/src/screens/aboutUs/AboutUs.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 
 function export default Home() {
 
+  return (
+
   <h1>you're home homie</h1>
+
+  )
 
 }
--- client/src/screens/home/Home.jsx
+++ client/src/screens/home/Home.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 
 export default function Home() {
+  return(
   <h1>you're home homie</h1>
+  )
 }
