ff2539bdf81eafaacc0d0f87edf0117ce15fb588
Kathleen B <kathleenb@Kathleens-MacBook-Air.local>
2020-08-21 14:44:31 -0400

    currently working on aboutus screen almost there!

--- client/src/components/layout/Layout.jsx
+++ client/src/components/layout/Layout.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import Nav from '../nav/Nav.jsx';
 
 const Layout = (props) => {
+  return (
   <div className="layout">
     <Nav />
     <div className="layout-children">{props.children}</div>
     <footer>
       <h4>© 2020 EHR. All rights reserved.</h4>
     </footer>
-  </div>
+    </div>
+  )
 }
 
 export default Layout;
