c0aa3eb218e0414dbe5d02d976ee537e0935571f
ajmonda <ajmonda@gmail.com>
2020-08-21 10:48:31 -0400

    Dummy text

---
+++ client/src/screens/aboutUs/AboutUs.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function export default Home() {
+
+  <h1>you're home homie</h1>
+
+}
---
+++ client/src/screens/home/Home.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function Home() {
+  <h1>you're home homie</h1>
+}
