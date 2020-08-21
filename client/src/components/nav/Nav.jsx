b955da68683566f0aa5cec03aadd150368fd45c2
Aiuner <demonspawndaemnyxea@gmail.com>
2020-08-21 10:11:24 -0400

    minor update to nav component

--- client/src/components/nav/Nav.jsx
+++ client/src/components/nav/Nav.jsx
@@ -4,19 +4,23 @@ import { Link } from "react-router-dom";
 const Nav = () => {
   return (
 
-    <nav>
-      <ul className="navLinks">
+    <>
+      <header>
+        <nav>
+          <ul className="navLinks">
 
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/aboutus">About Us</Link></li>
-        <li><Link to="/donate">Donate</Link></li>
-        <li><Link to="/404">Adopt</Link></li>
-        <li><Link to="/404">Volunteer</Link></li>
-        <li><Link to="/404">Foster</Link></li>
-        <li><Link to="/404">Events</Link></li>
-      
-      </ul>
-    </nav>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/aboutus">About Us</Link></li>
+            <li><Link to="/donate">Donate</Link></li>
+            <li><Link to="/404">Adopt</Link></li>
+            <li><Link to="/404">Volunteer</Link></li>
+            <li><Link to="/404">Foster</Link></li>
+            <li><Link to="/404">Events</Link></li>
+        
+          </ul>
+        </nav>
+      </header>
+    </>
 
   );
 }
