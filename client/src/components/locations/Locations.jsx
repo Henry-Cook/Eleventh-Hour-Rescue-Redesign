f3bc4d525605919ed2003ced64c53551dae1db7c
Kathleen B <kathleenb@Kathleens-MacBook-Air.local>
2020-08-23 15:45:03 -0400

    edited locations and added some css

--- client/src/components/locations/Locations.jsx
+++ client/src/components/locations/Locations.jsx
@@ -1,40 +1,77 @@
 import React from "react";
+import "./locations.css";
 
 export default function Locations() {
   return (
-    <div className='locations-container'>
+    <div className="locations-container">
       <div className="rockaway">
-        <h4>ROCKAWAY MALL</h4>
-        <p>301 Mt Hope Ave</p>
-        <p>Rockaway, NJ</p>
-        <p>Mon-Sat 10am-9:30pm</p>
-        <p>Sun 11am-6pm</p>
-        <p>973-970-0371</p>
-        <p>RockawayMallEHR@gmail.com</p>
+        <h4 className="locations-name">ROCKAWAY MALL</h4>
+        <p>
+          301 Mt Hope Ave
+          <br />
+          Rockaway, NJ
+        </p>
+        <p>
+          <strong>Mon-Sat</strong> 10am-9:30pm
+          <br />
+          <strong>Sun</strong> 11am-6pm
+        </p>
+        <p>
+          973-970-0371
+          <br />
+          RockawayMallEHR@gmail.com
+        </p>
       </div>
 
       <div className="EHRKennels">
-        <h5>EHR KENNELS</h5>
-        <p>(By appointment only. Not open to the public.)</p>
-        <p>861 Rt 10 East</p>
-        <p>Randolph,NJ</p>
-        <p>973-664-0865 (voicemail only) MainOffice@EHRdogs.org</p>
+        <h4 className="locations-name">
+          EHR KENNELS <br />
+          (By appointment only.
+          <br />
+          Not open to the public.)
+        </h4>
+        <p>
+          861 Rt 10 East
+          <br />
+          Randolph,NJ
+        </p>
+        <p>
+          973-664-0865
+          <br />
+          <strong>(voicemail only)</strong>
+          <br />
+          MainOffice@EHRdogs.org
+        </p>
       </div>
 
       <div className="kearney">
-        <h5>KEARNEY PETSMART</h5>
-        <p>160 Passaic Ave. Kearny, NJ</p>
-        <p>Mon-Sat 9am-9pm</p>
-        <p>Sun 10am-7pm</p>
-        <p>No direct phone</p>
+        <h4 className="locations-name">KEARNEY PETSMART</h4>
+        <p>
+          160 Passaic Ave.
+          <br />
+          Kearny, NJ
+        </p>
+        <p>
+          <strong>Mon-Sat</strong> 9am-9pm
+          <br />
+          <strong>Sun</strong> 10am-7pm
+        </p>
+        <p><strong>No direct phone</strong></p>
       </div>
 
       <div className="roxbury">
-        <h5>ROXBURY MALL</h5>
-        <p>275 Rt 10 East Succasunna,NJ</p>
-        <p>Sat 11am-4pm</p>
-        <p>Sun 11am-4pm</p>
-        <p>No direct phone</p>
+        <h4 className="locations-name">ROXBURY MALL</h4>
+        <p>
+          275 Rt 10 East
+          <br />
+          Succasunna,NJ
+        </p>
+        <p>
+          <strong>Sat</strong>11am-4pm
+          <br />
+          <strong>Sun</strong> 11am-4pm
+        </p>
+        <p><strong>No direct phone</strong></p>
       </div>
     </div>
   );
