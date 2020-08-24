b5d7ab9af2d4e9600b889a6c4de3446c6f09e1f3
Henry Cook <henrycook@Henrys-Air.lan>
2020-08-24 09:26:00 -0400

    Donate form

--- client/src/components/userInputs/UserInputs.jsx
+++ client/src/components/userInputs/UserInputs.jsx
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
 import "./userInputs.css";
+import states from "./state";
 
 function UserInputs() {
   const [userInfo, setUserInfo] = useState({
-    cardCardNumber: "",
+    number: "",
     cvv: "",
     expiration: "",
     firstName: "",
@@ -23,75 +24,149 @@ function UserInputs() {
     });
   };
 
+  const addSpace = (e) => {
+    let input = e.target.value;
+    if (input.length === 4 && e.keyCode !== 8) {
+      e.target.value = `${input} `;
+    } else if (input.length === 9 && e.keyCode !== 8) {
+      e.target.value = `${input} `;
+    } else if (input.length === 14 && e.keyCode !== 8) {
+      e.target.value = `${input} `;
+    }
+  };
+
   return (
     <>
       <div className="form-container">
         <form>
-          <div className="paypalChoice">
-            <input type="radio" />
-            <p>PayPal</p>
+          <div className="paypal-choice">
+            <input className="paypal-radio" type="radio" />
+            <p className="paypal-text">PayPal</p>
           </div>
-          <div className="creditCardChoice">
-            <input type="radio" />
-            <p>Credit Card</p>
+          <div className="credit-card-choice">
+            <input className="card-radio" type="radio" />
+
+            <p className="card-text">Credit Card or Debit Card</p>
           </div>
-          <div className="cardInformation">
+
+          <div className="card-information">
             <input
+              className="number"
               onChange={handleChange}
-              name="cardCardNumber"
-              type="password"
+              name="number"
+              onKeyDown={addSpace}
               required
-            />
-            <input
-              onChange={handleChange}
-              name="cvv"
+              placeholder="Credit Card Number"
               type="text"
-              required
-              onChange={handleChange}
+              maxlength="19"
+              autoComplete="off"
             />
-            <input
-              onChange={handleChange}
-              name="expiration"
-              type="text"
-              required
-            />
+            <div className="card-verify">
+              <input
+                className="verify-one"
+                onChange={handleChange}
+                name="cvv"
+                type="text"
+                required
+                onChange={handleChange}
+                placeholder="CVV"
+              />
+              <input
+                className="verify-two"
+                onChange={handleChange}
+                name="expiration"
+                type="text"
+                required
+                placeholder="MM/YY"
+              />
+            </div>
           </div>
-          <div className="billingInfo">
-            <h3>Billing Information</h3>
-            <input
-              onChange={handleChange}
-              type="text"
-              name="firstName"
-              required
-            />
-            <input
-              onChange={handleChange}
-              type="text"
-              name="lastName"
-              required
-            />
-            <input
-              onChange={handleChange}
-              type="text"
-              name="address"
-              required
-            />
-            <input
-              onChange={handleChange}
-              type="text"
-              name="address2"
-              required
-            />
-            <input onChange={handleChange} type="text" name="city" required />
-            <input onChange={handleChange} type="text" name="zip" required />
-            <select name="state" onChange={handleChange} required>
-              <option value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-            </select>
-            <input onChange={handleChange} type="email" required />
+          <div className="billing-info">
+            <h3 className="billing-info-title">Billing Information</h3>
+            <div className="inputs">
+              <div className="card-verify">
+                <input
+                  className="verify-one"
+                  onChange={handleChange}
+                  type="text"
+                  name="firstName"
+                  required
+                  placeholder="First Name"
+                />
+                <input
+                  className="verify-two"
+                  onChange={handleChange}
+                  type="text"
+                  name="lastName"
+                  required
+                  placeholder="Last Name"
+                />
+              </div>
+              <div className="card-verify">
+                <input
+                  className="verify-one"
+                  onChange={handleChange}
+                  type="text"
+                  name="address"
+                  required
+                  placeholder="Address"
+                />
+                <input
+                  className="verify-two"
+                  onChange={handleChange}
+                  type="text"
+                  name="address2"
+                  required
+                  placeholder="Address 2"
+                />
+              </div>
+              <div className="card-verify">
+                <input
+                  className="verify-one"
+                  onChange={handleChange}
+                  type="text"
+                  name="city"
+                  required
+                  placeholder="City"
+                />
+                <input
+                  className="verify-two"
+                  onChange={handleChange}
+                  type="text"
+                  name="zip"
+                  required
+                  placeholder="Zipcode"
+                />
+              </div>
+              <div className="card-verify">
+                <select
+                  className="state-select"
+                  name="state"
+                  onChange={handleChange}
+                  required
+                >
+                  <option value="">Select</option>
+                  {states.map((state) => {
+                    return <option value={state}>{state}</option>;
+                  })}
+                </select>
+              </div>
+              <div className="card-verify">
+                <input
+                  placeholder="Email Address"
+                  className="input-email"
+                  onChange={handleChange}
+                  type="email"
+                  required
+                />
+              </div>
+            </div>
           </div>
-          <button type="submit">Donate Now</button>
+          <div className="donate-btn-div">
+            <button className="donate-btn" type="submit">
+              DONATE NOW
+            </button>
+          </div>
         </form>
       </div>
     </>
