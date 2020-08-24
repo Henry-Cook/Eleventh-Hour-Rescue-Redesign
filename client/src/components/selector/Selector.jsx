89fdb538d3b306dae675e155d016f0dd2fa4ca67
Henry Cook <henrycook@Henrys-Air.lan>
2020-08-24 10:56:19 -0400

    Added Interactivity to the selector buttons

--- client/src/components/selector/Selector.jsx
+++ client/src/components/selector/Selector.jsx
@@ -6,8 +6,15 @@ function Selector(props) {
     type: "",
     amount: "",
   });
+  const [tabOneBool, setTabOneBool] = useState(false);
+  const [tabTwoBool, setTabTwoBool] = useState(false);
+  const [btnStyleValue, setBtnStyleValue] = useState("");
+
+  // let tabOneBool = false;
+  // let tabTwoBool = false;
 
   const handleTypeClick = (e) => {
+    console.log(tabOneBool);
     setChoices({
       ...choices,
       type: e.target.value,
@@ -25,58 +32,92 @@ function Selector(props) {
     <>
       <div className="selector-container">
         <div className="selector-tabs">
-          <button className="tab-one" onClick={handleTypeClick} value="monthly">
+          <button
+            className={tabOneBool === true ? "tab-one" : "tab-one-clicked"}
+            onClick={(e) => {
+              setTabOneBool(false);
+              setTabTwoBool(false);
+              handleTypeClick(e);
+            }}
+            value="monthly"
+          >
             Monthly
           </button>
-          <button className="tab-two" onClick={handleTypeClick} value="oneTime">
+          <button
+            className={tabOneBool === false ? "tab-two" : "tab-two-clicked"}
+            onClick={(e) => {
+              setTabTwoBool(true);
+              setTabOneBool(true);
+              handleTypeClick(e);
+            }}
+            value="oneTime"
+          >
             One-Time
           </button>
         </div>
         <div className="selector-btns">
           <div className="btn-grid">
             <button
-              className="btns"
-              onClick={handleAmountClick}
+              className={btnStyleValue === "5" ? "btns-clicked" : "btns"}
+              onClick={(e) => {
+                setBtnStyleValue("5");
+                handleAmountClick(e);
+              }}
               name="amount"
               value="5"
             >
               $5
             </button>
             <button
-              className="btns"
-              onClick={handleAmountClick}
+              className={btnStyleValue === "10" ? "btns-clicked" : "btns"}
+              onClick={(e) => {
+                setBtnStyleValue("10");
+                handleAmountClick(e);
+              }}
               name="amount"
               value="10"
             >
               $10
             </button>
             <button
-              className="btns"
-              onClick={handleAmountClick}
+              className={btnStyleValue === "25" ? "btns-clicked" : "btns"}
+              onClick={(e) => {
+                setBtnStyleValue("25");
+                handleAmountClick(e);
+              }}
               name="amount"
               value="25"
             >
               $25
             </button>
             <button
-              className="btns"
-              onClick={handleAmountClick}
+              className={btnStyleValue === "50" ? "btns-clicked" : "btns"}
+              onClick={(e) => {
+                setBtnStyleValue("50");
+                handleAmountClick(e);
+              }}
               name="amount"
               value="50"
             >
               $50
             </button>
             <button
-              className="btns"
-              onClick={handleAmountClick}
+              className={btnStyleValue === "100" ? "btns-clicked" : "btns"}
+              onClick={(e) => {
+                setBtnStyleValue("100");
+                handleAmountClick(e);
+              }}
               name="amount"
               value="100"
             >
               $100
             </button>
             <button
-              className="btns"
-              onClick={handleAmountClick}
+              className={btnStyleValue === "other" ? "btns-clicked" : "btns"}
+              onClick={(e) => {
+                setBtnStyleValue("other");
+                handleAmountClick(e);
+              }}
               name="amount"
               value="other"
             >
