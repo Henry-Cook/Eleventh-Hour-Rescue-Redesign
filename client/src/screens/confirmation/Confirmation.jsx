50d1375903376e5bcc7c48b7192188b12ad0385b
Henry Cook <henrycook@Henrys-Air.lan>
2020-08-25 17:45:36 -0400

    Added Confirmation Styling and CheckMark

--- client/src/screens/confirmation/Confirmation.jsx
+++ client/src/screens/confirmation/Confirmation.jsx
@@ -1,16 +1,19 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Layout from "../../components/layout/Layout";
 import { useParams } from "react-router-dom";
-import { getDonation } from "../../services/donations";
+import { getDonation, deleteDonation } from "../../services/donations";
+import { Checkmark } from "react-checkmark";
+import "./confirmation.css";
 
 export default function Confirmation() {
   let { id } = useParams();
-  console.log(id);
+  const [dontationData, setDonationData] = useState({});
 
   useEffect(() => {
+    window.scrollTo(0, 0);
     const apiCall = async () => {
       const data = await getDonation(id);
-      console.log(data);
+      setDonationData(data);
     };
     apiCall();
   }, []);
@@ -18,7 +21,11 @@ export default function Confirmation() {
   return (
     <>
       <Layout>
-        <h1>Confirmation</h1>
+        <div className="confirmation">
+          <Checkmark size={117} color="#00a1f8" />
+          <h2 className="thank-you">Thank You</h2>
+          <p className="confirmation-text">{`Your donation of $${dontationData.amount} has been received. We will send a confirmation email to the contact provided.  EHR will NOT send soliciting emails after your donation`}</p>
+        </div>
       </Layout>
     </>
   );
