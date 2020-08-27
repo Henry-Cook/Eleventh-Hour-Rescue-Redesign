911cf7f6b93604750217370d6a9bff9319cc0f57
ajmonda <ajmonda@gmail.com>
2020-08-27 02:23:07 -0400

    ternaries

--- client/src/components/stories/Stories.jsx
+++ client/src/components/stories/Stories.jsx
@@ -1,34 +1,47 @@
 import React, { useState, useEffect } from "react";
-import { getStories } from "../../services/stories";
+import { Link } from 'react-router-dom'
+import { getStories, getStory, updateStory } from "../../services/stories";
 import ActionCard from '../../components/action-card/ActionCard'
 import './stories.css'
 
 export default function Stories() {
 
   const [stories, updateStories] = useState([]);
+  const [editForm, setEditForm] = useState(false)
 
   useEffect(() => {
     const fetchStories = async () => {
-      const response = await getStories();
-      updateStories(response);
+      const response = await getStories(); updateStories(response);
     };
     console.log(stories)
     fetchStories();
   }, []);
 
+  const handleIt = (e) => {
+    e.preventDefault()
+    setEditForm(true)
+  }
+  
   return (
 
     
-      stories.map((story, i) => {
-        return (
-          <ActionCard
-            id={`story${i+1}`}
-            image={`${story.imgURL}`}
-            action={story.name}
-            detail={story.story} />
-        )
-      })
-
+    stories.map((story, i) => {
     
+      
+        !editForm ? (
+          <Link to={`/edit/${story._id}`}
+            onClick={handleIt} >
+            <ActionCard
+              id={`story${i + 1}`}
+              image={`${story.imgURL}`}
+              action={story.name}
+              detail={story.story} />
+          </Link > )
+         : <h1>h1</h1>)
+
+      
+    })
+  
+      
   )
 }
--- client/src/services/stories.js
+++ client/src/services/stories.js
@@ -18,15 +18,6 @@ export const getStory = async (id, story)  => {
   }
 }
 
-export const createStory = async () => {
-  try {
-    const response = await api.post('/story/new');
-    return response.data;
-  } catch (error) {
-    throw error;
-  }
-}
-
 export const updateStory = async (id, story) => {
   try {
     const response = await api.put(`/story/${id}`)
@@ -36,19 +27,23 @@ export const updateStory = async (id, story) => {
   }
 }
 
-export const deleteStory = async id => {
-  try {
-    const response = await api.delete(`/story/${id}`)
-    return response.data
-  } catch (error) {
-    throw error 
-  }
-}
 
-// // module.exports = {
-//   getStories,
-//   getStory,
-//   createStory,
-//   updateStory,
-//   deleteStory,
-// };
+// export const createStory = async () => {
+//   try {
+//     const response = await api.post('/story/new');
+//     return response.data;
+//   } catch (error) {
+//     throw error;
+//   }
+// }
+
+
+
+// export const deleteStory = async id => {
+//   try {
+//     const response = await api.delete(`/story/${id}`)
+//     return response.data
+//   } catch (error) {
+//     throw error 
+//   }
+// }
